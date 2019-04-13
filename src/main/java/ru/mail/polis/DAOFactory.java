4d21852336910dfe93a7bee22d93f119945af3f2
Egor.Shakhmin <lapter57@gmail.com>
2019-04-13 21:33:39 +0300

    added DAO implementation

--- src/main/java/ru/mail/polis/DAOFactory.java
+++ src/main/java/ru/mail/polis/DAOFactory.java
@@ -21,6 +21,8 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 
+import ru.mail.polis.shakhmin.DAOImpl;
+
 /**
  * Custom {@link DAO} factory.
  *
@@ -53,7 +55,6 @@ public final class DAOFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        // TODO: Implement me
-        throw new UnsupportedOperationException("Implement me!");
+        return new DAOImpl();
     }
 }
