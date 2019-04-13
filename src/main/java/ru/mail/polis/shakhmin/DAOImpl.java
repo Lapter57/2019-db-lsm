bb9a0a3d52121389a367812f6f6ce62243698e86
Egor.Shakhmin <lapter57@gmail.com>
2019-04-13 21:58:39 +0300

    issues were fixed

--- src/main/java/ru/mail/polis/shakhmin/DAOImpl.java
+++ src/main/java/ru/mail/polis/shakhmin/DAOImpl.java
@@ -16,22 +16,25 @@ public class DAOImpl implements DAO {
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
         return storage.tailMap(from).values().iterator();
     }
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+    public void upsert(
+            @NotNull final ByteBuffer key,
+            @NotNull final ByteBuffer value) throws IOException {
         storage.put(key, Record.of(key, value));
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) throws IOException {
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
         storage.remove(key);
     }
 
     @Override
     public void close() {
-
+        // without implementation
+        throw new UnsupportedOperationException();
     }
 }
